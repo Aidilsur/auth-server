30c1fff786b78f747268021c8bd0bcc1c0457501
aidil <aidil.surya@mandalafinance.com>
2024-02-16 14:51:05 +0700

    handle google

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -35,3 +35,50 @@ export const signin = async (req, res, next) => {
     next(error);
   }
 };
+
+export const google = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      const token = jwt.sign({ jd: user._id }, process.env.JWT_SECRET);
+      const { password: hashPassword, ...rest } = user._docs;
+      const expireDate = new Date(Date.now() + 3600000);
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+          expires: expireDate,
+        })
+        .status(200)
+        .json(rest);
+    } else {
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+
+      const hashPassword = bycriptjs.hashSync(generatedPassword, 10);
+
+      const newUser = new User({
+        username:
+          req.body.name.split(" ").join("").toLowerCase() +
+          Math.random().toString(36).slice(-8),
+        email: req.user.email,
+        password: hashPassword,
+        profilePicture: req.body.photo,
+      });
+
+      await newUser.save();
+      const token = jwt.sign({ jd: user._id }, process.env.JWT_SECRET);
+      const { password: hashPassword2, ...rest } = user._docs;
+      const expireDate = new Date(Date.now() + 3600000);
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+          expires: expireDate,
+        })
+        .status(200)
+        .json(rest);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
